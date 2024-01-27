8e92240556f3963b2d794567e602e13bb0f1cbde
Leo Liao <honglin0822@outlook.com>
2024-01-28 06:57:25 +0800

    :bug: Fix buid bug

--- src/app/system/__tests__/page.spec.tsx
+++ src/app/system/__tests__/page.spec.tsx
@@ -1,13 +1,11 @@
 import { render } from "@testing-library/react";
-import System, { maps } from "../page";
+import System from "../page";
 
 describe("#System", () => {
   it("should render link component successfully", () => {
     const { getByText } = render(<System />);
 
-    maps.forEach((item) => {
-      const link = getByText(item.name);
-      expect(link).toBeInTheDocument();
-    });
+    const link = getByText("Home");
+    expect(link).toBeInTheDocument();
   });
 });
